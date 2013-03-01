7fa5b5af87d166dd472540e5875fcc818569ee6b
John Martin <curmudgeon@rallydev.com>
2013-02-28 16:34:14 -0800

    add name, state, timebox dates for other side of arrow.  no build

--- App.js
+++ App.js
@@ -83,7 +83,62 @@ Ext.define('CustomApp', {
             }
         }
         me.log( ["rows",rows, "our_hash", me.our_hash ] );
-        me._getTimeboxes(type, rows);
+        me._getOtherData(type, rows);
+    },
+    _getOtherData: function(type, rows) {
+        var me = this;
+        me.log("_getOtherData (data from the other side of the dependency arrows " + type);
+        var item_length = rows.length;
+        var query = null;
+        for ( var i=0;i<item_length;i++ ) {
+            var item = rows[i];
+            if ( item.other_id ) {
+                var single_query = Ext.create('Rally.data.lookback.QueryFilter',{
+                    property: 'ObjectID', operator: '=', value: item.other_id
+                });
+                if ( query === null ) {
+                    query = single_query;
+                } else {
+                    query = query.or(single_query);
+                }
+            }
+        }
+        if ( query === null ) {
+            me._getTimeboxes(type,rows);
+        } else {
+            query = query.and(Ext.create('Rally.data.lookback.QueryFilter',{property: '__At', operator: '=',value: 'current' }));
+			Ext.create('Rally.data.lookback.SnapshotStore',{
+	            autoLoad: true,
+	            limit: 1000,
+	            fetch: ['Name','_ItemHierarchy', 'ScheduleState', 'Project', 'Iteration', 'Release' ],
+	            hydrate: [ 'ScheduleState' ],
+	            filters: query,
+	            listeners: {
+	                load: function( store, data, success ) {
+                        var data_length = data.length;
+                        for ( var i=0;i<data_length;i++ ) {
+                            me.other_hash[ data[i].get('ObjectID') ] = data[i].data;
+                        }
+                        me.log( ["other hash", me.other_hash ]);
+                        me._getTimeboxes(type,rows);
+	                }
+	            }
+	        });
+        }
+    },
+    _addToTimeboxFilter: function( query, value ) {
+        var single_query = Ext.create('Rally.data.QueryFilter', {
+           property: 'ObjectID',
+           operator: '=',
+           value: value
+        });
+        if ( ! query ) {
+            query = single_query;
+        } else {
+            query = query.or( single_query );
+        }
+        
+        return query;
     },
     _getTimeboxes: function( type, rows ) {
         var me = this;
@@ -93,36 +148,35 @@ Ext.define('CustomApp', {
         var query = null;
         for ( var i=0;i<item_length; i++ ) {
             if ( rows[i].release ) {
-                var single_query = Ext.create('Rally.data.QueryFilter', {
-                   property: 'ObjectID',
-                   operator: '=',
-                   value: rows[i].release
-                });
-                if ( ! query ) {
-                    query = single_query;
-                } else {
-                    query = query.or( single_query );
+                query = me._addToTimeboxFilter( query, rows[i].release );
+            }
+        }
+        for ( var i in me.other_hash ) {
+            if ( me.other_hash.hasOwnProperty(i) ) {
+                if ( me.other_hash[i].Release ) {
+                    query = me._addToTimeboxFilter(query, me.other_hash[i].Release );
                 }
             }
         }
         if ( query ) {
             Ext.create('Rally.data.WsapiDataStore',{
+                context: {project: null},
                 autoLoad: true,
                 model: 'Release',
                 fetch: [ 'ObjectID', 'ReleaseDate' ],
                 filters: query,
                 listeners: {
                     load: function( store, data, success ) {
-                        me.log( [ "release datastore", data ]);
                         var data_length = data.length;
                         for ( var i=0; i<data_length; i++ ) {
                             me.timebox_hash[ data[i].get('ObjectID') ] = { EndDate: data[i].get('ReleaseDate') };
                         }
-                        me.log( me.timebox_hash );
                         me._getIterations(type, rows);
                     }
                 }
             });
+        } else {
+            me._getIterations(type, rows);
         }
     },
     _getIterations: function(type,rows) {
@@ -133,32 +187,32 @@ Ext.define('CustomApp', {
         var query = null;
         for ( var i=0;i<item_length; i++ ) {
             if ( rows[i].iteration ) {
-                var single_query = Ext.create('Rally.data.QueryFilter', {
-                   property: 'ObjectID',
-                   operator: '=',
-                   value: rows[i].iteration
-                });
-                if ( ! query ) {
-                    query = single_query;
-                } else {
-                    query = query.or( single_query );
+                query = me._addToTimeboxFilter( query, rows[i].iteration );
+            }
+        }
+        for ( var i in me.other_hash ) {
+            if ( me.other_hash.hasOwnProperty(i) ) {
+                if ( me.other_hash[i].Iteration ) {
+                    query = me._addToTimeboxFilter(query, me.other_hash[i].Iteration );
                 }
             }
         }
+        me.log( query.toString() );
+        
         if ( query ) {
             Ext.create('Rally.data.WsapiDataStore',{
+                context: { project: null },
                 autoLoad: true,
                 model: 'Iteration',
                 fetch: [ 'ObjectID', 'EndDate' ],
                 filters: query,
                 listeners: {
                     load: function( store, data, success ) {
-                        me.log( [ "iteration datastore", data ]);
                         var data_length = data.length;
                         for ( var i=0; i<data_length; i++ ) {
                             me.timebox_hash[ data[i].get('ObjectID') ] = { EndDate: data[i].get('EndDate') };
                         }
-                        me.log( me.timebox_hash );
+                        
                         me._populateRowData(type,rows);
                     }
                 }
@@ -175,6 +229,19 @@ Ext.define('CustomApp', {
             }
             if (( item.release !== "" ) && ( this.timebox_hash[item.release] )) {
                 item.release_date = this.timebox_hash[item.release].EndDate;
+            }
+            
+            if ((item.other_id) && (this.other_hash[item.other_id])) {
+                var other = this.other_hash[item.other_id];
+                item.other_name = other.Name;
+                item.other_schedule_state = other.ScheduleState;
+            
+	            if (( other.Iteration ) && ( this.timebox_hash[other.Iteration] )) {
+	                item.other_iteration_date = this.timebox_hash[other.Iteration].EndDate;
+	            }
+	            if (( other.Release ) && ( this.timebox_hash[other.Release] )) {
+	                item.other_release_date = this.timebox_hash[other.Release].EndDate;
+	            }
             }
         }
         this._makeTable( type, rows );
