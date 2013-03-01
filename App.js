5ed4deda48ad7fba95440623b61da308467aad31
John Martin <curmudgeon@rallydev.com>
2013-02-28 20:33:19 -0800

    add project name on our side. No build.

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ Ext.define('CustomApp', {
     our_hash: {}, /* key is object id, content is the story from our project associated with that object id */
     other_hash: {}, /* key is object id, content is the story associated with that object id */
     timebox_hash: {}, /* key is object id of iteration or release. Changed both to have EndDate */
+    project_hash: {}, /* key is object id of projecs */
     launch: function() {
         var me = this;
         me._getBaseData();
@@ -26,11 +27,33 @@ Ext.define('CustomApp', {
      * 
      */
     _getBaseData: function() {
-        this._getTimeboxes();
+        this._getProjects();
     },
     _getDependencies: function() {
         this._getOurItems("Successors");
         this._getOurItems("Predecessors");
+    },
+    _getProjects: function() {
+        var me = this;
+        me.log( "_getProjects");
+        Ext.create('Rally.data.WsapiDataStore',{
+            context: {project: null},
+            autoLoad: true,
+            model: 'Project',
+            limit: 5000,
+            fetch: [ 'ObjectID', 'Name' ],
+            filters: { property: "State", operator: "!=", value: "Closed" },
+            listeners: {
+                load: function( store, data, success ) {
+                    var data_length = data.length;
+                    me.log( data_length );
+                    for ( var i=0; i<data_length; i++ ) {
+                        me.project_hash[ data[i].get('ObjectID') ] = { Name: data[i].get('Name') };
+                    }
+                    me._getTimeboxes();
+                }
+            }
+        });
     },
     _getTimeboxes: function() {
         var me = this;
@@ -82,7 +105,7 @@ Ext.define('CustomApp', {
     	Ext.create('Rally.data.lookback.SnapshotStore',{
     		autoLoad: true,
     		limit: 1000,
-    		fetch: ['Name','_ItemHierarchy',type, 'ScheduleState', 'Project', 'Iteration', 'Release' ],
+    		fetch: ['Name','_ItemHierarchy',type, 'ScheduleState', 'Project', 'Iteration', 'Release', '_UnformattedID' ],
             hydrate: [ 'ScheduleState' ],
     		filters: [ {
 	        	  property: '__At',
@@ -127,8 +150,8 @@ Ext.define('CustomApp', {
                     epic_report: "",
                     object_id: data[i].get('ObjectID'),
 		            direction: direction,
-		            project: 'tbd',
-		            name: data[i].get('Name'),
+		            project: data[i].get('Project'),
+		            name: "US" + data[i].get('_UnformattedID') + ": " + data[i].get('Name') ,
 		            schedule_state: data[i].get('ScheduleState'),
 		            release: data[i].get('Release'),
 		            iteration: data[i].get('Iteration'),
@@ -151,7 +174,6 @@ Ext.define('CustomApp', {
         }
         me.log( ["rows",rows, "our_hash", me.our_hash ] );
         me._getLeaves( type,rows );
-        //me._getOtherData(type, rows);
     },
 /**
  * having trouble when we have more than 300 items to look for at once
@@ -181,7 +203,6 @@ Ext.define('CustomApp', {
             property: 'Children', operator: '=', value: null
         }));
         query = query.and(Ext.create('Rally.data.lookback.QueryFilter',{property: '__At', operator: '=',value: 'current' }));
-        me.log( sliced_array.length );
         Ext.create('Rally.data.lookback.SnapshotStore',{
             autoLoad: true,
             limit: gap,
@@ -191,15 +212,12 @@ Ext.define('CustomApp', {
                 load: function( store, data, success ) {
                     var data_length = data.length;
                     me.log( "load leaves snapshot" );
-                    me.log( data );
                     for ( var i=0;i<data_length;i++ ) {
                         // only care if this is the child of one we already got
                         if ( data[i].get('_ItemHierarchy').length > 1 ) {
-                            me.log( data[i].get('_ItemHierarchy') );
                             var top_id = data[i].get('_ItemHierarchy')[0];
                             var bottom_id = data[i].get('ObjectID');
                             if ( me.our_hash[ top_id ] ) {
-                                me.log( "adding child to our: " + top_id );
                                 if ( ! me.our_hash[top_id].children ) {
                                     me.our_hash[top_id].scheduled_children = [];
                                     me.our_hash[top_id].children = [];
@@ -211,7 +229,6 @@ Ext.define('CustomApp', {
 	                                }
                                 }
                             } else if ( me.other_hash[top_id] ) {
-                                me.log( "in the other hash: " + top_id );
                                 if ( ! me.other_hash[top_id].children ) {
                                     me.other_hash[top_id].scheduled_children = [];
                                     me.other_hash[top_id].children = [];
@@ -223,7 +240,6 @@ Ext.define('CustomApp', {
                                     }
                                 }
                             } else {
-                                me.log( "not in the other hash: " + top_id );
                                 me.other_hash[top_id] = {
                                     scheduled_children: [],
                                     children: [bottom_id]
@@ -261,7 +277,6 @@ Ext.define('CustomApp', {
     },
     _doNestedOtherArray: function( type, rows, other_id_array, start_index ) {
         var me = this;
-        me.log( [ "_doNestedArray", start_index, other_id_array ] );
         var gap = 300;
         var sliced_array = other_id_array.slice(start_index, start_index + gap);
         
@@ -269,7 +284,6 @@ Ext.define('CustomApp', {
             property: 'ObjectID', operator: 'in', value: sliced_array
         });
         query = query.and(Ext.create('Rally.data.lookback.QueryFilter',{property: '__At', operator: '=',value: 'current' }));
-        me.log( sliced_array.length );
         Ext.create('Rally.data.lookback.SnapshotStore',{
             autoLoad: true,
             limit: gap,
@@ -279,7 +293,6 @@ Ext.define('CustomApp', {
             listeners: {
                 load: function( store, data, success ) {
                     var data_length = data.length;
-                    me.log( "load other snapshot" );
                     for ( var i=0;i<data_length;i++ ) {
                         if ( ! me.other_hash[data[i].get('ObjectID')] ) {
                             me.other_hash[ data[i].get('ObjectID') ] = data[i].data;
@@ -322,14 +335,21 @@ Ext.define('CustomApp', {
             if (( item.release !== "" ) && ( this.timebox_hash[item.release] )) {
                 item.release_date = this.timebox_hash[item.release].EndDate;
             }
+            if (( item.project ) && (this.project_hash[item.project])) {
+                this.log( this.project_hash[item.project] );
+                item.project = this.project_hash[item.project].Name;
+            } else { 
+                item.project = "Unknown " + item.project;
+            }
             
             if ( ( this.our_hash[ item.object_id ] ) && ( this.our_hash[item.object_id].children )) {
-                this.log( this.our_hash[item.object_id] );
                 var total_kids = this.our_hash[item.object_id].children.length;
                 var scheduled_kids = this.our_hash[item.object_id].scheduled_children.length;
                 item.epic = true;
                 item.epic_report = scheduled_kids + " of " + total_kids;
             }
+            
+            
             
             if ((item.other_id) && (this.other_hash[item.other_id])) {
                 var other = this.other_hash[item.other_id];
